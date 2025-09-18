b8a7d53b3d8aac7b0f05e19836cae3552727bda9
Daniel Aldemir <daniel.aldemir79@gmail.com>
2025-09-18 21:45:24 +0200

    while loop skapad som kör menyn tills man valt att avsluta. Även en switch case kopplad till menyn är skapad och förberedd för vidare kodning.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@
             const int pinkod = 1234;
             const int antalFörsök = 3;
 
+            Console.WriteLine("Välkommen till Bankomaten!");
+            Console.WriteLine();
+
             bool inloggad = LoggaIn(pinkod, antalFörsök);
             
             if (!inloggad)
@@ -18,20 +21,57 @@
             }
             
             Console.WriteLine();
-            Console.WriteLine("Välkommen till Bankomaten!");
+            
 
-        
-        
-        
-        
-        
-        
+            
+            int val = 0;
+
+            while (val != 4)
+            {
+
+                ShowMenu();
+                val = GetInt();
+
+                switch (val)
+                {
+                    case 1:
+                        Console.WriteLine("Du har valt att sätta in pengar.");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Du har valt att ta ut pengar.");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Du har valt att visa saldo.");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Du har valt att avsluta.");
+                        break;
+                    default:
+                        Console.WriteLine("Du har angett felaktigt val");
+                        break;
+                }
+            }
+           
+
+
+
         }
 
         
        
-        
-        
+        static void ShowMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1. Sätt in pengar");
+            Console.WriteLine("2. Ta ut pengar");
+            Console.WriteLine("3. Visa saldo");
+            Console.WriteLine("4. Avsluta");
+            Console.WriteLine();
+        }
+
         static bool LoggaIn(int pinKod, int antalFörsök)
         {
             int försökKvar = antalFörsök;
@@ -41,12 +81,14 @@
                 int angivenKod = GetInt();
                 if (angivenKod == pinKod)
                 {
+                    Console.WriteLine();
                     Console.WriteLine("Inloggning lyckades!");
                     return true;
                 }
                 else
                 {
                     försökKvar--;
+                    Console.WriteLine();
                     Console.WriteLine($"Felaktig pinkod. Du har {försökKvar} försök kvar.");
                 }
             }
